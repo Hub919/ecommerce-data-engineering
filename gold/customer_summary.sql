4c65237cf0fa8a5ff66d89c1edc669ff6e765c53
Hub919 <bawazirabdallah160@gmail.com>
2025-06-17 17:52:16 +0530

    Update customer_summary.sql

--- gold/customer_summary.sql
+++ gold/customer_summary.sql
@@ -6,5 +6,6 @@ SELECT
     COUNT(DISTINCT customer_id) AS total_customers,
     COUNT(email) AS total_emails,
     COUNT(*) AS total_records
+-- Table reference assumes Delta table already written from PySpark job
 FROM 
     silver.cleaned_customers;
